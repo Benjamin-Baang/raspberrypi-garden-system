f1aea6174944d5f489850edab4c954e69350c07f
Benjamin-Baang <benjaminbaang@gmail.com>
2022-01-13 00:39:59 +0000

    Added main script and pseudocode

---
+++ main.py
@@ -0,0 +1,10 @@
+#####    Pseudocode    #####
+# Read sensor values
+# Convert sensor readings to acceptable values
+# Take picture with camera
+# Calculate average NDVI value(s) from picture
+# Use values to determine activation
+# Send values to database
+
+##### TODO Later #####
+# Incorporate different modes of operations
